1297ec88320c8b6f6abdc0d7e800cc75a213dbb7
Anirudha Nanda <anirudhananda@Anirudhas-MacBook-Pro.local>
2020-05-17 19:25:15 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+state = {
+  persons : [
+    { name: 'Ani', age: 28},
+    { name: 'Harsh', age: 29},
+    { name: 'Vivek', age: 24}
+  ]
+}
+  render() {
+
+    return (
+      <div className="App">
+      <h1>Hello</h1>
+      <button>Switch Name</button>
+
+    <Person  name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies : raching</Person>
+    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+
+
+  }
+}
+
+export default App;
